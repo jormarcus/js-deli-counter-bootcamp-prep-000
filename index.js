868b2cc5b78c29bd34a77107830034403dcb12f8
Jordan Marcus <jormarcus92@gmail.com>
2018-02-03 16:14:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function takeANumber(katzDeliLine, name) {
   var katzDeli = [];
-  for (var i = 0; i < katzDeli.length; i++) {
-    if (katzDeliLine[i] === name) {
-      console.log('Welcome, ' + name + ' You are number ' + (i + 1) + ' in line.');
-    }
-  }
+  katzDeliLine.push(name);
+  return ('Welcome, ' + name + ' You are number ' + katzDeliLine.length + ' in line.');
 }
